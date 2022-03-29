bf5bb7a5b492a86957eac9b45ff7d986cce1e861
Luguito <gomezfrancisco@gmail.com>
2022-03-29 13:56:54 -0500

    install(emotion): emotion/styled emotion/react
    feat(login): page login finish

--- global-styled/global-styled.ts
+++ global-styled/global-styled.ts
@@ -1,15 +1,54 @@
 import styled from 'styled-components';
+import { PrimaryBlueColor, PrimaryGreyColor } from './global-colors';
+import Button from '@mui/material/Button';
 
 export const ContainerCenter = styled.section`
     display: flex; 
     justify-content: center;
     align-items: center;
     height: 100%;
-    background-color: #f1f5f9;
+    background-color: ${PrimaryBlueColor};
 `;
 
 export const CardContainer = styled.article`
     background-color: #FFFFFF;
     min-width: 10em;
     min-height: 10em;
+    color: ${PrimaryGreyColor};
+`;
+
+export const HeaderCard = styled.header`
+    display: flex;
+    margin-bottom: 1.5em;
+`;
+
+export const TitleCard = styled.label`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ContentCard = styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+`
+
+export const ItemCard = styled.label`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const FooterCard = styled.article`
+    display: flex;
+    align-items: center;
+    margin-top: 2em;
+`
+
+export const ButtonGrey = styled(Button)`
+    && {
+        background-color: ${PrimaryGreyColor};
+        border-radius: 20px;
+        color: #FFFFFF;
+    }
 `
--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,11 +1,31 @@
 import { } from 'react';
-import { CardContainer, ContainerCenter } from '@global-styled';
+import { CardContainer, ContainerCenter, ContentCard, FooterCard, HeaderCard, TitleCard, ItemCard, ButtonGrey } from '@global-styled';
+import { TextField } from '@mui/material';
 
 export const LoginComponent = () => {
     return (
         <ContainerCenter>
-            <CardContainer>
-                
+            <CardContainer style={{ padding: '2em', width: '25%' }}>
+                <HeaderCard>
+                    <TitleCard>
+                        <h3 style={{ marginBottom: '8px', color: 'black' }}>Iniciar sesión</h3>
+                        <small>Ingresa tus credenciales</small>
+                    </TitleCard>
+                </HeaderCard>
+                <ContentCard>
+                    <ItemCard>
+                        Cedula
+                        <TextField size="small" />
+                    </ItemCard>
+                    <ItemCard>
+                        Contraseña
+                        <TextField size="small" />
+                    </ItemCard>
+                    <small style={{ textAlign: 'right' }}>Olvide mi contraseña</small>
+                </ContentCard>
+                <FooterCard style={{ justifyContent: 'center' }}>
+                    <ButtonGrey style={{ width: '90%' }}>Iniciar sesión</ButtonGrey>
+                </FooterCard>
             </CardContainer>
         </ContainerCenter>
     )
