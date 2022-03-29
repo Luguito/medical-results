13776f859251c6ee2be6ef41a9eac136a6290925
Luguito <gomezfrancisco@gmail.com>
2022-03-29 12:40:39 -0500

    feat(styles): global styles height 100%
    feat(login): Initial Login pages
    
    feat(global)

--- global-styled/global-styled.ts
+++ global-styled/global-styled.ts
@@ -1,1 +1,15 @@
 import styled from 'styled-components';
+
+export const ContainerCenter = styled.section`
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: #f1f5f9;
+`;
+
+export const CardContainer = styled.article`
+    background-color: #FFFFFF;
+    min-width: 10em;
+    min-height: 10em;
+`
---
+++ pages/login/login.tsx
@@ -0,0 +1,12 @@
+import { } from 'react';
+import { CardContainer, ContainerCenter } from '@global-styled';
+
+export const LoginComponent = () => {
+    return (
+        <ContainerCenter>
+            <CardContainer>
+                
+            </CardContainer>
+        </ContainerCenter>
+    )
+}
