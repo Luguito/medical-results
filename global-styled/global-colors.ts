1088cad217d60244bf2a074906a86df89d132e56
Luguito <gomezfrancisco@gmail.com>
2022-03-29 13:47:43 -0500

    feat(color): file with all global colors

---
+++ global-styled/global-colors.ts
@@ -0,0 +1,5 @@
+export const PrimaryGreyColor = "#818181";
+export const SecondaryGreyColor = "#c4c4c4";
+export const PrimaryBlueColor = "#f1f5f9";
+export const LayoutColor = "#363740";
+export const LetterLayoutColor = "#a4a6b3";
