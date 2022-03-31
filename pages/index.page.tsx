673a038f5a63d7acae60d28c6e269181c2a84591
Luguito <gomezfrancisco@gmail.com>
2022-03-31 18:13:05 -0500

    rename: main pages

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
-import { LoginComponent } from './login/login.page'
-const Home: NextPage = () => {
+import { LoginComponent } from './login/index.page'
+
+export const Home: NextPage = () => {
   return (
     <main style={{ height: '100%' }}>
       <LoginComponent></LoginComponent>
