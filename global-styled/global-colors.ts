01e6b92b592b6a9d5f41042c6c416b91b5e983d6
Luguito <gomezfrancisco@gmail.com>
2022-03-31 20:44:52 -0500

    feat(color): new color for items in list

--- global-styled/global-colors.ts
+++ global-styled/global-colors.ts
@@ -3,3 +3,4 @@ export const SecondaryGreyColor = "#c4c4c4";
 export const PrimaryBlueColor = "#f1f5f9";
 export const LayoutColor = "#363740";
 export const LetterLayoutColor = "#a4a6b3";
+export const ItemListColor = "#46474f"
