c2ea930b5410731cda48a6116cfddbf7d99c3d58
Luguito <gomezfrancisco@gmail.com>
2022-05-05 09:56:35 -0500

    titles dashboard

--- pages/app/dashboard/index.page.tsx
+++ pages/app/dashboard/index.page.tsx
@@ -8,9 +8,9 @@ import { Perfiles } from '../api';
 export const DashboardPage = () => {
     const [cards, setCards] = useState([]);
     const labels = {
-        'totalAdmins': 'Admins',
-        'totalPatients': 'Pacientes',
-        'totalCups': 'Codigos cup',
+        'totalAdmins': 'No. Usuarios Admin',
+        'totalPatients': 'No. Usuarios pacientes',
+        'totalCups': 'No. Codigos CUP Activos',
         'totalOrderByStatus': {
             'O': 'ORDENADO',
             'E': 'EN PROCESO',
@@ -51,13 +51,12 @@ export const DashboardPage = () => {
                             })}
                         </div>
                         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                            <h3 style={{ color: TextColor }}>Total resultados ultimos 4 meses</h3>
-                            <h3 style={{ color: TextColor }}>Usuarios y Cups activos</h3>
+                            <h3 style={{ color: TextColor }}>Examenes realizados en los ultimos 4 meses</h3>
                         </div>
                         <div style={{ display: 'flex', gap: '1.5em', }}>
                             {/* @ts-ignore */}
                             <RenderLineChart totalByMount={cards['totalByMount']}></RenderLineChart>
-                            <div style={{ display: 'flex', gap: '1.5em', flexDirection: 'column' }}>
+                            <div style={{ display: 'flex', gap: '1em', flexDirection: 'column' }}>
                                 {cards && Object.keys(cards).map((item, index) => {
                                     return (
                                         <>
@@ -90,7 +89,7 @@ export default DashboardPage;
 export const RenderLineChart = ({ totalByMount }: { totalByMount: any[] }) => {
     return (
         <>
-            <LineChart width={1000} height={500} data={totalByMount} margin={{ top: 30, right: 20, bottom: 5, left: 0 }} style={{ backgroundColor: 'white', borderRadius: '10px' }}>
+            <LineChart width={950} height={500} data={totalByMount} margin={{ top: 30, right: 20, bottom: 5, left: 0 }} style={{ backgroundColor: 'white', borderRadius: '10px' }}>
                 <Line type="monotone" dataKey="value" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="name" />
@@ -105,7 +104,7 @@ export const BoxChart = ({ title, content }: { title: string, content: string })
     return (
         <>
             <Box sx={{
-                width: 150,
+                width: 200,
                 height: 120,
                 backgroundColor: 'white',
                 borderRadius: '10px',
@@ -114,7 +113,7 @@ export const BoxChart = ({ title, content }: { title: string, content: string })
                 flexDirection: 'column',
                 alignItems: 'center'
             }}>
-                <h3 style={{ color: TextColor }}>{title}</h3>
+                <h5 style={{ color: TextColor }}>{title}</h5>
                 <p style={{ color: '#252733', fontWeight: 400, fontSize: '1.4rem', margin: 0 }}>{content}</p>
             </Box>
         </>
